f678749bfc022f25d12744e1e95d7e807f964f7e
prem.bikram.limbu <prem.bikram.limbu@gmail.com>
2022-01-22 00:05:42 +0545

    Added faq template to render by executeTemplate function

--- main.go
+++ main.go
@@ -37,8 +37,7 @@ func contactHandler(w http.ResponseWriter, r *http.Request) {
 }
 
 func faqHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-type", "text/html; charset=utf-8")
-	fmt.Fprint(w, "<p>Q: Is there a free version?<br>A: Yes, we offer 30 days in any paid plans.</p><p>Q: What are your hours?<br>A: We support 24/7 hours, though response time may be slower during weekends.</p><p>Q: How do I contact support?<br>A: Email us : godev-care@godev.com</p>")
+	executeTemplate(w, filepath.Join("templates", "faq.gohtml"))
 }
 
 func main() {
