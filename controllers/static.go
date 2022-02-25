f685fc2dca1bbe0a2a2c06c2710966cacd6e1ba2
prem.bikram.limbu <prem.bikram.limbu@gmail.com>
2022-02-25 15:49:28 +0545

    Dynamic FAQ page

--- controllers/static.go
+++ controllers/static.go
@@ -1,6 +1,7 @@
 package controllers
 
 import (
+	"html/template"
 	"net/http"
 
 	"github.com/codecamp-prem/goDev/views"
@@ -10,5 +11,33 @@ import (
 func StaticHandler(tpl views.Template) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		tpl.Execute(w, nil)
+	}
+}
+
+// FAQ : dynamic faq
+func FAQ(tpl views.Template) http.HandlerFunc {
+	questions := []struct {
+		Question string
+		Answer   template.HTML
+	}{
+		{
+			Question: "Is there a free version?",
+			Answer:   "Yes, we offer 30 days in any paid plans.",
+		},
+		{
+			Question: "What are your hours?",
+			Answer:   "We support 24/7 hours, though response time may be slower during weekends.",
+		},
+		{
+			Question: "How do I contact support?",
+			Answer:   `Email us : <a href="mailto:support@google.com">godev_care@godev.com</a>`,
+		},
+		{
+			Question: "Any hidden charges?",
+			Answer:   "No! It is what you see in pricing list.",
+		},
+	}
+	return func(w http.ResponseWriter, r *http.Request) {
+		tpl.Execute(w, questions)
 	}
 }
--- views/template.go
+++ views/template.go
@@ -35,14 +35,16 @@ func Parse(filepath string) (Template, error) {
 	}, nil
 }
 
+//Template Struct
 type Template struct {
 	htmlTpl *template.Template
 }
 
+// Execute tpl and data
 func (t Template) Execute(w http.ResponseWriter, data interface{}) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 
-	err := t.htmlTpl.Execute(w, nil)
+	err := t.htmlTpl.Execute(w, data)
 	if err != nil {
 		log.Printf("Executing template: %v", err)
 		http.Error(w, "There was an error Executing the template", http.StatusInternalServerError)
